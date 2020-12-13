e55c57fa8e1e4e7f62d32231b35f461a1b1356e0
mchaikov <m.chaikov@artsofte.ru>
2020-12-13 19:58:37 +0500

    game2

--- main.js
+++ main.js
@@ -1,174 +1,104 @@
 "use strict";
 
-// объект с уровнями сложности
-const LEVEL = {
-    easy: 3,
-    medium: 5,
-    hard: 7
+// объект с размерами полей
+const SIZE = {
+    small: { width: 3, hight: 2 },
+    medium: { width: 4, hight: 2 },
+    big: { width: 4, hight: 3 }
 };
 
-// объект с цветами
-const COLORS = {
-    mistake: 'radial-gradient(circle, rgba(252,136,69,1) 0%, rgba(253,29,29,1) 100%)',
-    correct: 'radial-gradient(circle, rgba(0,212,255,1) 0%, rgba(59,138,240,1) 100%)',
-    default: 'linear-gradient(#d1d1d1, rgb(156, 156, 156))'
-};
+// массив ссылок на картинки
+const pictures = [
+    'https://images.unsplash.com/photo-1607714724990-8f5495b75883?crop=entropy&cs=srgb&fm=jpg&ixid=MXwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHw&ixlib=rb-1.2.1&q=85',
+    'https://images.unsplash.com/photo-1605256108216-7e4a97ac9d93?crop=entropy&cs=srgb&fm=jpg&ixid=MXwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHw&ixlib=rb-1.2.1&q=85',
+    'https://images.unsplash.com/photo-1605894646636-bab3db312b65?crop=entropy&cs=srgb&fm=jpg&ixid=MXwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHw&ixlib=rb-1.2.1&q=85',
+    'https://images.unsplash.com/photo-1605839406808-a6043334fa32?crop=entropy&cs=srgb&fm=jpg&ixid=MXwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHw&ixlib=rb-1.2.1&q=85',
+    'https://images.unsplash.com/photo-1606065258119-27297317b812?crop=entropy&cs=srgb&fm=jpg&ixid=MXwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHw&ixlib=rb-1.2.1&q=85',
+    'https://images.unsplash.com/photo-1607002944680-233705405df0?crop=entropy&cs=srgb&fm=jpg&ixid=MXwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHw&ixlib=rb-1.2.1&q=85',
+];
 
-// возвращает случайное число от 0 до max
+// гвернет случайное число от 0 до мax
 const getRandom = (max) => Math.floor(Math.random() * Math.floor(max));
 
-// создаем последовательность, в которуй нужно нажать на шары
-const generateSequence = (level) => {
-    let sequence = [];
-    for (let i = 0; i < level; i++) {
-        sequence.push(getRandom(level));
-    }
-    return sequence;
-};
+// вернет перемешенный массив картинок, каждая встречается 2 раза
+const mixImgs = (pictures, size) => {
+    let pairsArray = [];
 
-// подсвечиваем элемент
-const lightUp = (item, color) => {
-    item.style.background = color
-}
+    // в массиве будут лежать цифры, которые показывает, сколько раз повторялась картинка (1, 2, либо undefined)
+    let qtyArr = [];
 
-// подсвечиваем все элементы
-const lightUpAll = (items, color) => {
-    for (const item of items) {
-        lightUp(item, color)
-    }
-}
+    // этот цикл для массива
+    for (let i = 0; i < size.width * size.hight; i++) {
 
-// мигаем элементом
-const blink = (item, color, prevColor, onend) => {
-    let previousBackground = prevColor === undefined ? item.style.background : prevColor
-    lightUp(item, color)
-    setTimeout(() => {
-        lightUp(item, previousBackground);
-        onend()
-    }, 200);
-};
+        // если в массиве qtyArr под текущим индексом находится 2, то цикл начинается сначала, пока не выберет картинку, которая повторялась 1 раз или нисколько
+        while (true) {
 
-// мигаем всеми элементом
-const blinkAll = (items, color, prevColor) => {
-    for (const item of items) {
-        blink(item, color, prevColor)
-    }
-};
+            let index = getRandom((size.width * size.hight) / 2);
+            if (!pairsArray[i]) {
 
-// положение элемента относительно окна
-const getOffset = (el) => {
-    const rect = el.getBoundingClientRect();
-    return {
-        left: rect.left + window.scrollX,
-        top: rect.top + window.scrollY
-    };
-}
+                if (!qtyArr[index])
+                    qtyArr[index] = 1;
+                else if (qtyArr[index] >= 2)
+                    continue;
+                else
+                    qtyArr[index]++;
 
-// генерация координат элементов
-const getCoordinates = (container, ctx, items) => {
-    let left = getRandom(container.clientWidth)
-    let top = getRandom(container.clientHeight)
-    let pixel = ctx.getImageData(left, top, 1, 1)
-    let pixelData = pixel.data
-    if (pixelData[3] === 0) {
-        return getCoordinates(container, ctx, items)
-    }
-    for (const ip of items) {
-        if (getOffset(ip).left == 0 || getOffset(ip).top == 0) {
-            continue
-        }
-        if (Math.abs(getOffset(ip).left - left) < 30 || Math.abs(getOffset(ip).top - top) < 30) {
-            return getCoordinates(container, ctx, items)
+                pairsArray[i] = pictures[index];
+                break;
+            }
         }
     }
+    return pairsArray;
+};
 
-    return { left: left, top: top }
-}
-
-// создаем поле и возвращаем его элементы
-let createField = (level) => {
+// банально создаем поле, 
+let createField = (picturesArr, size) => {
     let container = document.querySelector('.container');
-    let image = new Image()
-    image.crossOrigin = '*'
-    let ctx = container.getContext("2d")
-    image.onload = (e) => {
-        ctx.drawImage(image, 0, 0)
-        image.style.display = "none"
-        let items = document.querySelectorAll('.item')
-        for (let i = 0; i < level; i++) {
-            let coordinates = getCoordinates(container, ctx, items)
-            items[i].style.left = `${coordinates.left}px`
-            items[i].style.top = `${coordinates.top}px`
-        }
-    }
-    image.src = "res/new-year-tree.png"
 
-    let items = []
-    let aboveCanvas = document.querySelector('.above-canvas')
-    for (let i = 0; i < level; i++) {
-        let item = document.createElement('div');
-        item.className = 'item';
-        items.push(item)
-        aboveCanvas.append(item);
+    let row = document.createElement('div');
+    row.className = 'row';
+
+    let item = document.createElement('div');
+    item.className = 'item';
+
+    let content = document.createElement('div');
+    content.className = 'item_show';
+
+    let img = document.createElement('img');
+    img.className = 'item_img';
+
+    let items = [];
+
+    for (let i = 0; i < size.hight; i++) {
+        let currentRow = row.cloneNode();
+        for (let j = 0; j < size.width; j++) {
+            let currentImg = img.cloneNode();
+            currentImg.src = picturesArr[i * size.width + j];
+
+            let currentContent = content.cloneNode();
+            currentContent.append(currentImg);
+
+            let currentItem = item.cloneNode();
+            currentItem.append(currentContent);
+
+            currentRow.append(currentItem);
+            items.push(currentItem);
+        }
+        container.append(currentRow);
     }
     return items;
-};
+}
 
-// начинаем
-let start = (level) => {
-    const items = createField(level);
-
-    let sequence = generateSequence(level);
-    let currentIndex = 0;
-    let currentNumber = sequence[currentIndex];
-    let isWin = false;
-
-    console.log(sequence)
+let start = () => {
+    let size = SIZE.big;
+    let images = mixImgs(pictures, size)
+    let items = createField(images, size);
 
     for (let i = 0; i < items.length; i++) {
         items[i].addEventListener('click', (e) => {
-            if (isWin)
-                return;
-            console.log(currentNumber)
-
-            if (i == currentNumber) {
-                lightUp(items[i], COLORS.correct)
-                currentNumber = sequence[++currentIndex];
-                if (currentIndex == sequence.length) {
-                    isWin = true;
-                    blinkAll(items, COLORS.correct, COLORS.default);
-                    setTimeout(() => alert('победа'), 500);
-                }
-            } else {
-                blink(items[i], COLORS.mistake, COLORS.default, () => { lightUpAll(items, COLORS.default) });
-                currentIndex = 0;
-                currentNumber = sequence[0];
-            }
-        });
+            items[i].classList.toggle('show');
+        })
     }
 }
 
-// очищение поля
-const clearField = () => {
-    let aboveCanvas = document.querySelector('.above-canvas')
-    while (aboveCanvas.firstChild) {
-        aboveCanvas.removeChild(aboveCanvas.lastChild)
-    }
-}
-
-document.querySelector('#easy-game-btn').onclick = (e) => {
-    clearField()
-    start(LEVEL.easy)
-}
-
-document.querySelector('#medium-game-btn').onclick = (e) => {
-    clearField()
-    start(LEVEL.medium)
-}
-
-document.querySelector('#hard-game-btn').onclick = (e) => {
-    clearField()
-    start(LEVEL.hard)
-}
-
-start(LEVEL.easy);
+start();
